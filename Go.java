e28859a92da7760e3579c8379f571e43a1dd5f97
Jason Shi <jasonxshi2002@gmail.com>
2017-06-12 10:17:27 -0700

    Update Go.java

--- Go.java
+++ Go.java
@@ -10,17 +10,19 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
-public class Go implements MouseListener, ActionListener{
-	int tableSize = 20; //size 19 x 19 (plus border)
-	boolean[][] squares = new boolean[tableSize][tableSize]; //each array is table size x table size
-	boolean [][] nextsquares = new boolean [tableSize][tableSize];
+public class Go implements MouseListener, ActionListener {
+	int tableSize = 20; // size 19 x 19 (plus border)
+	boolean[][] squares = new boolean[tableSize][tableSize]; // each array is
+																// table size x
+																// table size
+	boolean[][] nextsquares = new boolean[tableSize][tableSize];
 	int neighbors = 0;
 	JFrame frame = new JFrame("Go");
-	GoPanel panel = new GoPanel(squares); //grid in middle
-	Container north = new Container(); //space on side
-	Container east = new Container(); //space on side
-	Container south = new Container(); //space on side
-	Container west = new Container(); //space on side
+	GoPanel panel = new GoPanel(squares); // grid in middle
+	Container north = new Container(); // space on side
+	Container east = new Container(); // space on side
+	Container south = new Container(); // space on side
+	Container west = new Container(); // space on side
 	JButton back = new JButton("Take Back");
 	JButton pass = new JButton("Pass Turn");
 	JLabel turn = new JLabel("                                 Turn: Black");
@@ -31,19 +33,20 @@ public class Go implements MouseListener, ActionListener{
 	static boolean moved = false;
 	Piece first = null;
 	ArrayList<ArrayList<Piece>> completed = new ArrayList<ArrayList<Piece>>();
-	public Go(){
+
+	public Go() {
 		frame.setSize(800, 800);
 		frame.setResizable(true);
 		frame.setLayout(new BorderLayout());
 		frame.add(panel, BorderLayout.CENTER);
 		panel.addMouseListener(this);
-		//south container
-		south.setLayout((new GridLayout(1,3)));
+		// south container
+		south.setLayout((new GridLayout(1, 3)));
 		south.add(back);
-		back.setBackground(new Color(230,230,230));
+		back.setBackground(new Color(230, 230, 230));
 		back.addActionListener(this);
 		south.add(pass);
-		pass.setBackground(new Color(230,230,230));
+		pass.setBackground(new Color(230, 230, 230));
 		pass.addActionListener(this);
 		south.add(turn);
 		frame.add(south, BorderLayout.SOUTH);
@@ -51,53 +54,88 @@ public class Go implements MouseListener, ActionListener{
 		frame.setVisible(true);
 		frame.setResizable(false);
 	}
+
 	public static void main(String[] args) {
 		new Go();
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource().equals(back)){
-			//back();
+		if (e.getSource().equals(back)) {
+			// back();
 		}
-		if (e.getSource().equals(pass)){
+		if (e.getSource().equals(pass)) {
 			moved = !moved;
-			if (moved == true){
+			if (moved == true) {
 				turn.setText("                                 Turn: White");
 			}
-			if(moved == false){
+			if (moved == false) {
 				turn.setText("                                 Turn: Black");
 			}
 		}
 	}
+
 	@Override
-	public void mouseClicked(MouseEvent e) {}
-	@Override
-	public void mouseEntered(MouseEvent e) {}
-	@Override
-	public void mouseExited(MouseEvent e) {}
-	@Override
-	public void mousePressed(MouseEvent e) {}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		System.out.println(e.getX() + " , " + e.getY()); 
-		double width = (double)panel.getWidth() / squares[0].length; //width and height of lines
-		double height = (double)panel.getHeight() / squares[0].length; 
-		
-		int column = Math.min(24,(int)Math.round(e.getX() / width)); //number of columns and rows closest to click
-		int row = Math.min(24, (int)Math.round(e.getY() / height));
-		if (column == 0 ||  row == 0 || column == 20 ||  row == 20){
-			return;
-		}
-		panel.addPiece((int)(column*width), (int)(row*height));
-		
-		moved = !moved;
-		if (moved == true){
-			turn.setText("                                 Turn: White");
-		}
-		if(moved == false){
-			turn.setText("                                 Turn: Black");
-		}
-		frame.repaint();
+	public void mouseClicked(MouseEvent e) {
 	}
 
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		System.out.println(e.getX() + " , " + e.getY());
+		double width = (double) panel.getWidth() / squares[0].length; // width
+																		// and
+																		// height
+																		// of
+																		// lines
+		double height = (double) panel.getHeight() / squares[0].length;
+
+		int column = Math.min(24, (int) Math.round(e.getX() / width)); // number
+																		// of
+																		// columns
+																		// and
+																		// rows
+																		// closest
+																		// to
+																		// click
+		int row = Math.min(24, (int) Math.round(e.getY() / height));
+		if (column == 0 || row == 0 || column == 20 || row == 20) {
+			return;
+		}
+
+		boolean valid = true;
+		for (int i = 0; i < panel.getPieceList().size(); i++) {
+			if (panel.getPieceList().get(i).getX() == (int) (column * width)
+					&& panel.getPieceList().get(i).getY() == (int) (row * height)) {
+				valid = false;
+				break;
+			}
+		}
+		if (valid) {
+			moved = !moved;
+			if (moved == true) {
+				turn.setText("                                 Turn: White");
+				panel.addPiece((int) (column * width), (int) (row * height), "black");
+
+			} else if (moved == false) {
+				turn.setText("                                 Turn: Black");
+				panel.addPiece((int) (column * width), (int) (row * height), "white");
+			}
+		} else {
+			// invalid piece
+		}
+		frame.repaint();
+
+	}
 }
